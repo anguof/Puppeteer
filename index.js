9006123f9396972747a790fe71e59eb0f350d81b
anguo <guo.an@surpath.net.cn>
2020-12-18 11:31:47 +0800

    model

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const puppeteer = require("puppeteer");
-// const CREDS = require("./creds"); // 登录用
 const fs = require("fs");
+
+const mongoose = require("mongoose");
+const Movie = require("./models/movie");
 
 const ITEM_LIST = ".item";
 const MOVE_NAME = ".hd>a:first-child";
 const MOVE_SCORE = ".rating_num";
+
+const DB_URL = "mongodb://localhost/movie";
+if (mongoose.connection.readyState == 0) {
+  mongoose.connect(
+    DB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("connected to " + DB_URL);
+      }
+    }
+  );
+}
+
+// 入库
+function upsertMovie(movieObj) {
+  Movie.create(movieObj, (err, result) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
 
 // 爬虫
 async function run() {
@@ -14,7 +40,7 @@ async function run() {
   const page = await browser.newPage();
 
   let users = [];
-  for (let i = 0; i <= 10; i++) {
+  for (let i = 0; i <= 2; i++) {
     // 跳转到指定页码
     await page.goto(`https://movie.douban.com/top250?start=${i * 25}&filter=`, {
       waitUntil: "domcontentloaded",
@@ -43,20 +69,25 @@ async function run() {
     );
     users = [...users, ...itemUsers];
   }
-
-  console.log(users);
-
-  if (!fs.existsSync("./data")) {
-    fs.mkdirSync("./data");
+  for (let i = 0, length = users.length; i < length; i++) {
+    upsertMovie({
+      name: users[i]["name"],
+      score: users[i]["score"],
+      dateCrawled: new Date(),
+    });
   }
+
+  // if (!fs.existsSync("./data")) {
+  //   fs.mkdirSync("./data");
+  // }
   // 将数据以.json格式储存在data文件夹下
-  fs.writeFile(
-    `./data/movie.json`,
-    JSON.stringify({ movieList: users }),
-    (err) => {
-      if (err) throw err;
-    }
-  );
+  // fs.writeFile(
+  //   `./data/movie.json`,
+  //   JSON.stringify({ movieList: users }),
+  //   (err) => {
+  //     if (err) throw err;
+  //   }
+  // );
 }
 
 run();
