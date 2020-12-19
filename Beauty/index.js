ab3f252ce697d41158147be3bf17912818a5b902
anguo <anguo@surpath.net>
2020-12-19 17:33:07 +0800

    download webp

--- Beauty/index.js
+++ Beauty/index.js
@@ -7,37 +7,56 @@ const IMAGE_ITEM = "img";
 // 爬虫
 async function run() {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
 
   let users = [];
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 5; i++) {
     // 跳转到指定页码
-    await page.goto(`https://movie.douban.com/celebrity/1018562/photos/?type=C&start=${i * 30}&sortby=like&size=a&subtype=a`, {
-      waitUntil: "networkidle0",
-      timeout: 0,
-    });
+    await page.goto(
+      `https://movie.douban.com/celebrity/1014295/photos/?type=C&start=${
+        i * 30
+      }&sortby=like&size=a&subtype=a`,
+      {
+        waitUntil: "networkidle0",
+        timeout: 0,
+      }
+    );
 
     const itemUsers = await page.evaluate(
       (sel, moveName) => {
         return Array.prototype.slice
           .apply(document.querySelectorAll(sel))
           .map(($userListItem) => {
-            const name = $userListItem
-              .querySelector(moveName)
-              .src;
+            const name = $userListItem.querySelector(moveName).src;
             return {
-              name
+              name,
             };
           });
       },
       IMAGE_LIST,
-      IMAGE_ITEM,
+      IMAGE_ITEM
     );
 
     users = [...users, ...itemUsers];
-    console.log(users);
+  }
+
+  if (!fs.existsSync("./public")) {
+    fs.mkdirSync("./public");
+  }
+
+  for (let i = 0, length = users.length; i < length; i++) {
+    const viewSource = await page.goto(users[i]["name"]);
+    fs.writeFile(
+      `./public/No${i + 1}.webp`,
+      await viewSource.buffer(),
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+      }
+    );
   }
 
   await browser.close();
